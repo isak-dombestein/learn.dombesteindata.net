189e7d07b73ace1c7bf828c948b24239d2efd303
Isak Dombestein <isak@dombesteindata.net>
2025-04-07 04:47:31 +0200

    removed cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
   vite: {
       plugins: [tailwindcss()]
 	},
